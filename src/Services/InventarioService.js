e5ede761aa38f4e5953777e54d9b03906ad02396
ClaraEst <estefania.jimenez@est.iudigital.edu.co>
2022-06-12 15:43:04 -0500

    Se agrega el proyecto

---
+++ src/Services/InventarioService.js
@@ -0,0 +1,29 @@
+import { axiosConfig } from '../Helpers/axios-config';
+
+const getInventarios = () => {
+    return axiosConfig.get('inventario/listar',{
+        headers:{
+            'Content-type': 'application/json'
+        }
+    });
+}
+
+const crearInventarios = (data) => {
+    return axiosConfig.post('inventario/guardar',data,{
+        headers:{
+            'Content-type': 'application/json'
+        }
+    });
+}
+
+const editInventarios = (inventarioId, data) => {
+    return axiosConfig.put(`inventario/editar/${inventarioId}`,data,{
+        headers:{
+            'Content-type': 'application/json'
+        }
+    });
+}
+
+export {
+    getInventarios, crearInventarios, editInventarios
+}
